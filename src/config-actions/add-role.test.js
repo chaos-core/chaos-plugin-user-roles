3739da4a1b0fb419531808a2679407c8837eeef5
Stephen A. Wilson <Stephen-356@hotmail.com>
2020-04-09 11:34:34 -0400

    remove calls to testConfigAction

--- src/config-actions/add-role.test.js
+++ src/config-actions/add-role.test.js
@@ -1,90 +1,98 @@
 const Discord = require('discord.js');
+const {MockMessage} = require("chaos-core").test.discordMocks;
 
 const createChaosBot = require('../../test/create-chaos-bot');
 
-describe('Config: AddRoleAction', function () {
-  beforeEach(function () {
+describe('Config: addRole', function () {
+  beforeEach(async function () {
     this.chaos = createChaosBot();
-    this.test$ = this.chaos.testConfigAction({
-      pluginName: 'userRoles',
-      actionName: 'addRole',
-    });
+    this.message = new MockMessage();
+    await this.chaos.listen().toPromise();
+    await this.chaos.getService('core', 'PermissionsService')
+      .addUser(this.message.guild, 'admin', this.message.author)
+      .toPromise();
+    await this.chaos.getService('core', 'PluginService')
+      .enablePlugin(this.message.guild.id, 'UserRoles')
+      .toPromise();
   });
 
-  context('when a role is not given', function () {
+  describe('!config userRoles addRole', function () {
+    beforeEach(function () {
+      this.message.content = '!config userRoles addRole';
+    });
+
     it('gives a help message', async function () {
-      const response = await this.test$.toPromise();
-      expect(response).to.containSubset({
+      const responses = await this.chaos.testMessage(this.message);
+      expect(responses[0]).to.containSubset({
         content: "I'm sorry, but I'm missing some information for that command:",
       });
     });
   });
 
-  const roleId = Discord.SnowflakeUtil.generate();
-  const roleName = 'testRole';
+  describe('!config userRoles addRole {role}', function () {
+    const roleId = Discord.SnowflakeUtil.generate();
+    const roleName = 'testRole';
 
-  Object.entries({
-    "as a mention": `<@${roleId}>`,
-    "as an alt mention": `<@&${roleId}>`,
-    "by name": roleName,
-    "by id": roleId,
-  }).forEach(([type, roleString]) => {
-    context(`when a role is given ${type}`, function () {
-      beforeEach(function () {
-        this.test$.args.role = roleString;
-      });
+    Object.entries({
+      "as a mention": `<@${roleId}>`,
+      "as an alt mention": `<@&${roleId}>`,
+      "by name": roleName,
+      "by id": roleId,
+    }).forEach(([type, roleString]) => {
+      context(`when a role is given ${type}`, function () {
+        beforeEach(function () {
+          this.message.content = `!config userRoles addRole ${roleString}`;
+        });
 
-      context('when the role can not be found', function () {
-        it('gives a user friendly error', async function () {
-          const response = await this.test$.toPromise();
-          expect(response).to.containSubset({
-            status: 400,
-            content: `The role '${roleString}' could not be found`,
+        context('when the role can not be found', function () {
+          it('gives a user friendly error', async function () {
+            const responses = await this.chaos.testMessage(this.message);
+            expect(responses[0]).to.containSubset({
+              content: `The role '${roleString}' could not be found`,
+            });
           });
         });
-      });
 
-      context('when the role exists', function () {
-        context(type, function () {
-          beforeEach(function () {
-            this.role = {
-              id: roleId,
-              name: roleName,
-              guild: this.test$.message.guild,
-            };
-            this.test$.message.guild.roles.set(this.role.id, this.role);
-          });
+        context('when the role exists', function () {
+          context(type, function () {
+            beforeEach(function () {
+              this.role = {
+                id: roleId,
+                name: roleName,
+                guild: this.message.guild,
+              };
+              this.message.guild.roles.set(this.role.id, this.role);
+            });
 
-          context('when the role has not been added', function () {
-            it('gives a success message', async function () {
-              const response = await this.test$.toPromise();
-              expect(response).to.containSubset({
-                status: 200,
-                content: `Users can now join ${roleName}`,
+            context('when the role has not been added', function () {
+              it('gives a success message', async function () {
+                const responses = await this.chaos.testMessage(this.message);
+                expect(responses[0]).to.containSubset({
+                  content: `Users can now join ${roleName}`,
+                });
+              });
+
+              it('marks the role as joinable', async function () {
+                const UserRolesService = this.chaos.getService('UserRoles', 'UserRolesService');
+                sinon.spy(UserRolesService, 'allowRole');
+
+                await this.chaos.testMessage(this.message);
+                expect(UserRolesService.allowRole).to.have.been.calledWith(this.role);
               });
             });
 
-            it('marks the role as joinable', async function () {
-              const UserRolesService = this.chaos.getService('UserRoles', 'UserRolesService');
-              sinon.spy(UserRolesService, 'allowRole');
+            context('when the role has already been added', function () {
+              beforeEach(async function () {
+                await this.chaos.getService('UserRoles', 'UserRolesService')
+                  .allowRole(this.role)
+                  .toPromise();
+              });
 
-              await this.test$.toPromise();
-              expect(UserRolesService.allowRole).to.have.been.calledWith(this.role);
-            });
-          });
-
-          context('when the role has already been added', function () {
-            beforeEach(async function () {
-              await this.chaos.getService('UserRoles', 'UserRolesService')
-                .allowRole(this.role)
-                .toPromise();
-            });
-
-            it('gives a user friendly message', async function () {
-              const response = await this.test$.toPromise();
-              expect(response).to.containSubset({
-                status: 400,
-                content: `Users can already join ${roleName}.`,
+              it('gives a user friendly message', async function () {
+                const responses = await this.chaos.testMessage(this.message);
+                expect(responses[0]).to.containSubset({
+                  content: `Users can already join ${roleName}.`,
+                });
               });
             });
           });
--- src/config-actions/remove-role.test.js
+++ src/config-actions/remove-role.test.js
@@ -1,90 +1,98 @@
 const Discord = require('discord.js');
+const {MockMessage} = require("chaos-core").test.discordMocks;
 
 const createChaosBot = require('../../test/create-chaos-bot');
 
-describe('Config: RemoveRoleAction', function () {
-  beforeEach(function () {
+describe('Config: removeRole', function () {
+  beforeEach(async function () {
     this.chaos = createChaosBot();
-    this.test$ = this.chaos.testConfigAction({
-      pluginName: 'userRoles',
-      actionName: 'removeRole',
-    });
+    this.message = new MockMessage();
+    await this.chaos.listen().toPromise();
+    await this.chaos.getService('core', 'PermissionsService')
+      .addUser(this.message.guild, 'admin', this.message.author)
+      .toPromise();
+    await this.chaos.getService('core', 'PluginService')
+      .enablePlugin(this.message.guild.id, 'UserRoles')
+      .toPromise();
   });
 
-  context('when a role is not given', function () {
+  describe('!config userRoles removeRole', function () {
+    beforeEach(function () {
+      this.message.content = '!config userRoles removeRole';
+    });
+
     it('gives a help message', async function () {
-      const response = await this.test$.toPromise();
-      expect(response).to.containSubset({
+      const responses = await this.chaos.testMessage(this.message);
+      expect(responses[0]).to.containSubset({
         content: "I'm sorry, but I'm missing some information for that command:",
       });
     });
   });
 
-  const roleId = Discord.SnowflakeUtil.generate();
-  const roleName = 'testRole';
+  describe('!config userRoles removeRole {role}', function () {
+    const roleId = Discord.SnowflakeUtil.generate();
+    const roleName = 'testRole';
 
-  Object.entries({
-    "as a mention": `<@${roleId}>`,
-    "as an alt mention": `<@&${roleId}>`,
-    "by name": roleName,
-    "by id": roleId,
-  }).forEach(([type, roleString]) => {
-    context(`when a role is given ${type}`, function () {
-      beforeEach(function () {
-        this.test$.args.role = roleString;
-      });
+    Object.entries({
+      "as a mention": `<@${roleId}>`,
+      "as an alt mention": `<@&${roleId}>`,
+      "by name": roleName,
+      "by id": roleId,
+    }).forEach(([type, roleString]) => {
+      context(`when a role is given ${type}`, function () {
+        beforeEach(function () {
+          this.message.content = `!config userRoles removeRole ${roleString}`;
+        });
 
-      context('when the role can not be found', function () {
-        it('gives a user friendly error', async function () {
-          const response = await this.test$.toPromise();
-          expect(response).to.containSubset({
-            status: 400,
-            content: `The role '${roleString}' could not be found`,
+        context('when the role can not be found', function () {
+          it('gives a user friendly error', async function () {
+            const responses = await this.chaos.testMessage(this.message);
+            expect(responses[0]).to.containSubset({
+              content: `The role '${roleString}' could not be found`,
+            });
           });
         });
-      });
 
-      context('when the role exists', function () {
-        context(type, function () {
-          beforeEach(function () {
-            this.role = {
-              id: roleId,
-              name: roleName,
-              guild: this.test$.message.guild,
-            };
-            this.test$.message.guild.roles.set(this.role.id, this.role);
-          });
-
-          context('when the role has been added', function () {
-            beforeEach(async function () {
-              await this.chaos.getService('UserRoles', 'UserRolesService')
-                .allowRole(this.role)
-                .toPromise();
+        context('when the role exists', function () {
+          context(type, function () {
+            beforeEach(function () {
+              this.role = {
+                id: roleId,
+                name: roleName,
+                guild: this.message.guild,
+              };
+              this.message.guild.roles.set(this.role.id, this.role);
             });
 
-            it('gives a success message', async function () {
-              const response = await this.test$.toPromise();
-              expect(response).to.containSubset({
-                status: 200,
-                content: `Users can no longer join ${roleName}`,
+            context('when the role has been added', function () {
+              beforeEach(async function () {
+                await this.chaos.getService('UserRoles', 'UserRolesService')
+                  .allowRole(this.role)
+                  .toPromise();
+              });
+
+              it('gives a success message', async function () {
+                const responses = await this.chaos.testMessage(this.message);
+                expect(responses[0]).to.containSubset({
+                  content: `Users can no longer join ${roleName}`,
+                });
+              });
+
+              it('marks the role as not joinable', async function () {
+                this.UserRolesService = this.chaos.getService('UserRoles', 'UserRolesService');
+                sinon.spy(this.UserRolesService, 'removeRole');
+
+                await this.chaos.testMessage(this.message);
+                expect(this.UserRolesService.removeRole).to.have.been.calledWith(this.role);
               });
             });
 
-            it('marks the role as not joinable', async function () {
-              this.UserRolesService = this.chaos.getService('UserRoles', 'UserRolesService');
-              sinon.spy(this.UserRolesService, 'removeRole');
-
-              await this.test$.toPromise();
-              expect(this.UserRolesService.removeRole).to.have.been.calledWith(this.role);
-            });
-          });
-
-          context('when the role has not been added', function () {
-            it('gives a user friendly message', async function () {
-              const response = await this.test$.toPromise();
-              expect(response).to.containSubset({
-                status: 400,
-                content: `Users could not join ${roleName}.`,
+            context('when the role has not been added', function () {
+              it('gives a user friendly message', async function () {
+                const responses = await this.chaos.testMessage(this.message);
+                expect(responses[0]).to.containSubset({
+                  content: `Users could not join ${roleName}.`,
+                });
               });
             });
           });
